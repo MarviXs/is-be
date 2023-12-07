cf2c0cf97e997ee449fd6d9db8310cd2e71a8b52
Michal Rendek <rendekm.michal@gmail.com>
2023-12-07 10:06:26 +0100

    feat: collection init

--- src/main/java/sk/stuba/sdg/isbe/repositories/CollectionRepository.java
+++ src/main/java/sk/stuba/sdg/isbe/repositories/CollectionRepository.java
@@ -1,8 +1,8 @@
 package sk.stuba.sdg.isbe.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import sk.stuba.sdg.isbe.domain.model.Module;
+import sk.stuba.sdg.isbe.domain.model.Collection;
 
-public interface CollectionRepository extends MongoRepository<Module, String> {
+public interface CollectionRepository extends MongoRepository<Collection, String> {
 
 }
