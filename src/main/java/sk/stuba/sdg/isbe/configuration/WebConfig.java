d98d7e93edb67ba5395bbb70e63b94111bff74ac
Marek Šmogrovič <xsmogrovic@stuba.sk>
2024-01-14 21:13:09 +0100

    fix

--- src/main/java/sk/stuba/sdg/isbe/configuration/WebConfig.java
+++ src/main/java/sk/stuba/sdg/isbe/configuration/WebConfig.java
@@ -1,5 +1,7 @@
 package sk.stuba.sdg.isbe.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableMethodSecurity
@@ -18,22 +22,31 @@ public class WebConfig {
     private JwtAuthFilter jwtAuthFilter;
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // Allow all origins
+        configuration.setAllowedMethods(Arrays.asList("*")); // Allow all methods
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.cors().and()
+                .authorizeHttpRequests()
                 .requestMatchers("/api/user/create/**").permitAll()
                 .requestMatchers("/api/user/login/**").permitAll()
                 .requestMatchers("/swagger-ui/**").permitAll()
                 .requestMatchers("/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated() // All other requests must be authenticated
                 .and()
-                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()) 
-                .and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // No session will be created by Spring Security
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // No session will be created by Spring
+                                                                         // Security
 
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // Add JWT filter
 
-        http.cors().and();
         // http.csrf().disable();
         return http.build();
     }
